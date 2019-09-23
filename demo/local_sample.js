c980165705a3e11a5c0bb51e95367a695761b9e6
eugeniosegala <eugeniosegalaweb@gmail.com>
2019-09-23 23:37:42 +0100

    template local storage

---
+++ demo/local_sample.js
@@ -0,0 +1,12 @@
+const localStorage = [
+  {
+    name: 'my-awesome-data',
+    data: '{"version":"2.29.2","data":{"device":"xxxx-xxxx-xxxx-xxxx-xxxx"},"expires":false}',
+  },
+  {
+    name: 'other-awesome-data',
+    data: '{"version":"2.29.2","data":{"device":"xxxx-xxxx-xxxx-xxxx-xxxx"},"expires":false}',
+  }
+];
+
+module.exports = localStorage;
